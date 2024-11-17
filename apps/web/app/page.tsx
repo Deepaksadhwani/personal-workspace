44d9455540c651e06463206d4390ddc75c42bbb9
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-11-17 08:49:43 +0530

    initiate personal-workspaces for personal projects

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="text-2xl text-rose-700">
+    <Button variant="destructive">Click me</Button>
+    hello world
+   </div>
+  );
+}
