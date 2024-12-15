1c5b6c26d2c99f58658aaa15c76ecc3a383a1b7b
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-12-15 17:01:52 +0530

    feat(jira-clone): implement authentication routes Add login, registration, and logout functionality - chore: Add environment variable examples for Appwrite configuration and Update API URL and add Appwrite client types

--- apps/jira-clone/lib/rpc.ts
+++ apps/jira-clone/lib/rpc.ts
@@ -3,5 +3,5 @@ import { hc } from "hono/client";
 
 // Create type-safe HTTP client instance
 export const client = hc<AppType>(
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:7000",
 );
---
+++ packages/ui/src/types/global.ts
@@ -0,0 +1,16 @@
+export type AppwriteClientTypes = {
+  endpoint: string;
+  projectId: string;
+  apiKey: string;
+};
+
+export type createUserAppwriteTypes = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+export type LoginUserAppwriteTypes = {
+  email: string;
+  password: string;
+};
