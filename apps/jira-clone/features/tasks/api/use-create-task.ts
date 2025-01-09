217359037a462afd0e10d2b94454e6c5c43e6058
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2025-01-09 22:01:55 +0530

    feat(jira-clone): create get and post api endpoint and form to create task

---
+++ apps/jira-clone/features/tasks/api/use-create-task.ts
@@ -0,0 +1,32 @@
+import { client } from "@/lib/rpc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+// Extract response and request types from the login endpoint
+type ResponseType = InferResponseType<(typeof client.api.tasks)["$post"], 200>;
+type RequestType = InferRequestType<(typeof client.api.tasks)["$post"]>;
+
+export const useCreateTask = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json }) => {
+      const response = await client.api.tasks.$post({ json });
+
+      if (!response.ok) {
+        throw new Error("Failed to create task");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("task created successfully");
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: () => {
+      toast.error("Failed to create task");
+    },
+  });
+  return mutation;
+};
